d682c7cb408bd896266cbd82e2d88195a3d3276a
Людмила <lyuda14070702@icloud.com>
2025-05-12 19:46:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo  # Додаємо WebAppInfo
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage

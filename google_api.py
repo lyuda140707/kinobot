1b845f9bcef50dbd852c1d903af87b777510a290
Людмила <lyuda14070702@icloud.com>
2025-05-27 16:33:07 +0300

    Update google_api.py

--- google_api.py
+++ google_api.py
@@ -2,6 +2,9 @@ import gspread
 import os
 import json
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+
 
 def get_gsheet_data():
     creds_json = os.getenv("GOOGLE_SHEETS_CREDENTIALS_JSON")
@@ -14,3 +17,11 @@ def get_gsheet_data():
     sheet_id = os.getenv("SHEET_ID")
     sheet = client.open_by_key(sheet_id).sheet1
     return sheet.get_all_records()
+
+def get_google_service():
+    creds_dict = json.loads(os.getenv("GOOGLE_SHEETS_CREDENTIALS_JSON"))
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+
+    service = build("sheets", "v4", credentials=creds)
+    return service

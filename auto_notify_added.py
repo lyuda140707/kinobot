f52d0d417385c8be0bf1e3adb39517109d776fa1
Людмила <lyuda14070702@icloud.com>
2025-06-15 12:16:11 +0300

    Update auto_notify_added.py

--- auto_notify_added.py
+++ auto_notify_added.py
@@ -43,8 +43,14 @@ async def check_and_notify():
 
         try:
             keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                [InlineKeyboardButton(text="☕ Підтримати КіноБота", url="https://send.monobank.ua/jar/2FdmSYjoGo")],
-                [InlineKeyboardButton(text="📣 Запросити друга", url="https://t.me/UAKinoTochka_bot")]
+                [InlineKeyboardButton(
+                    text="☕ Підтримати КіноБота",
+                    url="https://send.monobank.ua/jar/2FdmSYjoGo"
+                )],
+                [InlineKeyboardButton(
+                    text="📣 Запросити друга",
+                    url="https://t.me/share/url?url=https://t.me/UAKinoTochka_bot&text=🔎 Знайди будь-який фільм українською в @UAKinoTochka_bot!"
+                )]
             ])
 
             msg = await bot.send_message(

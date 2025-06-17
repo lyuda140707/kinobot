725c3fe373deb27312fd334303a37ac8ec7ad0e1
Людмила <lyuda14070702@icloud.com>
2025-06-17 18:12:26 +0300

    Update auto_notify_added.py

--- auto_notify_added.py
+++ auto_notify_added.py
@@ -46,12 +46,10 @@ async def check_and_notify():
 
         try:
             keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    
                 [InlineKeyboardButton(text="☕ Підтримати КіноБота", url="https://send.monobank.ua/jar/2FdmSYjoGo")],
                 [InlineKeyboardButton(text="📋 Скопіювати запрошення", switch_inline_query="Дивись фільми українською — @UAKinoTochka_bot")],
-        
-                🎬 Відкрити КіноБот WebApp", web_app=WebAppInfo(url="https://your-webapp-link.com"))]
-
+                [InlineKeyboardButton(text="🎬 Відкрити КіноБот WebApp", web_app=WebAppInfo(url="https://your-webapp-link.com"))]
+    
             ])
             msg = await bot.send_message(
                 chat_id=int(user_id),

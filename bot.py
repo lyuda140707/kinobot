3450e044de24e6c24676ffcac6e13514fda611fa
Людмила <lyuda14070702@icloud.com>
2025-08-07 19:12:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ async def approve_pro(message: types.Message):
     print("✅ Стартував approve_pro")
     print(f"From user id: {message.from_user.id}, text: {message.text}")
 
-    if message.from_user.id not in [8265377605, 7963871119]
+    if message.from_user.id not in [8265377605, 7963871119]:
         print("⛔ Не твій адмінський ID, вихід.")
         return
 
